aec6e33fbffb8659125cc9e73db9954f2879ce73
Nevena Stoeva <nevena.stoeva@gmail.com>
2020-04-10 15:27:30 -0700

    Amends config pointer

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 moment = Moment(app)
-app.config.from_object('config.SQLALCHEMY_DATABASE_URI')
+app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
