cec43dc2f860df845a10a8bfa301839ab67f3172
Nevena Stoeva <nevena.stoeva@gmail.com>
2020-04-11 21:36:25 -0700

    Upgrades Form to FlaskForm (per deprecation warning)

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from forms import *
 from flask_migrate import Migrate
 
