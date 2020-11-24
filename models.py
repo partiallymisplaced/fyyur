d5f884181ed4575b70938fb9795abe961ef67ca0
Nevena Stoeva <nevena.stoeva@gmail.com>
2020-11-24 13:35:51 -0800

    Removes unnecessary app import in models

--- models.py
+++ models.py
@@ -1,11 +1,9 @@
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 class Venue(db.Model):
     __tablename__ = 'venues'
